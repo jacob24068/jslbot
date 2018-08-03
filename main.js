24795dedf2b7d4644ed998591db069e1dc10596d
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2018-08-03 17:29:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -28,6 +28,11 @@ const save = function() {
     })
 }
 
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
