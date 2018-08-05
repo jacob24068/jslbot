a07b693ef1cf1f4db5fe109c13807fbd795eed91
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2018-08-04 20:20:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ const color = function(message, args){
         }else{
             message.member.addRole(role)
         }
+        message.channel.send("👍")
     }else{
         message.channel.send("that's not a color we do here, !colors")
     }
@@ -91,14 +92,90 @@ const colorslist = function(message){
 })
 }
 
-const rentroom = function(message, args){
+let usersmakingrooms = {}
+let waitingforinput = {}
+let chosennames = {}
 
+const rentroom = function(message, args){
+    let authorid = message.author.id
+    if (authorid != '188386891182112769') return
+    if (waitingforinput[authorid]) return message.channel.send({"embed": {
+        title: "You are already creating a room, type !cancel to restart.",
+        color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+    }})
+    if (saveData[authorid].room) return message.channel.send({"embed": {
+        title: "You are already have a room. Type !manage to manage your current room.",
+        color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+    }})
+    waitingforinput[authorid] = "name"
+    message.channel.send({"embed": {
+        title: "Please enter a name for the room.",
+        color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+    }})
+}
+
+const roominput = function(message, args){
+    const authorid = message.author.id
+    const content = message.content
+    const state = waitingforinput[authorid]
+    if (state == "name"){
+        chosennames[authorid] = content
+        message.channel.send({"embed": {
+            title: "Confirm name",
+            description: content + " (reply with yes/no)",
+            color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+        }})
+    }else if (state == "confirmname") {
+        if (!chosennames[authorid]) return
+        if (!content.toLowerCase().match("no") || !content.toLowerCase().match("yes")) return
+        if (content.toLowerCase().match("no")) {
+            waitingforinput[authorid] = "name"
+            message.channel.send({"embed": {
+                title: "Please enter a name for the room.",
+                color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+            }})
+        }else{
+            waitingforinput[authorid] = "type"
+            message.channel.send({"embed": {
+                title: "Choose room type",
+                description: "(voice/text/both)",
+                color: Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+            }})
+        }
+    }else if (state == "type") {
+        if (!chosennames[authorid]) return
+        if (!content.toLowerCase().match("voice") || !content.toLowerCase().match("text") || !content.toLowerCase().match("text")) return
+        const voice = content.toLowerCase().match("voice") || content.toLowerCase().match("both")
+        const text = content.toLowerCase().match("text") || content.toLowerCase().match("both")
+        message.guild.createRole(chosennames[authorid]).then(function(role){
+            message.user.addRole(role)
+            message.guild.createChannel(chosennames[authorid], "text", [{
+                id: message.guild.id,
+                deny: ['READ_MESSAGES', 'SEND_MESSAGES']
+            },
+            {
+                id: role.id,
+                allow: ['READ_MESSAGES', 'SEND_MESSAGES']
+            }
+            ])
+            message.guild.createChannel(chosennames[authorid], "voice", [{
+                id: message.guild.id,
+                deny: ['VIEW_CHANNEL', 'CONNECT']
+            },
+            {
+                id: role.id,
+                allow: ['VIEW_CHANNEL', 'CONNECT']
+            }
+            ])
+        })
+
+    }
 }
 
 const aliases = {
     "color": color,
     "colors": colorslist,
-    "rentroom": rentroom
+    "rent": rentroom
 }
 
 discordClient.on("message", message => {
@@ -106,5 +183,6 @@ discordClient.on("message", message => {
     if (message.content.substr(0,prefix.length) != prefix) return
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase()
+    if (waitingforinput[message.author.id]) return roominput(message, args)
     if (aliases[command]) return aliases[command](message, args)
 })
